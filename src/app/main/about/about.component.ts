6c7486a7d372d5057ac580cece96b8c0c6cfaf6f
JuanCarlos Aguilera <jcaaguilerat@gmail.com>
2020-12-10 17:32:03 -0300

    Updated formatting

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import localize from 'nativescript-localize';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import localize from "nativescript-localize";
 
 @Component({
-  selector: 'ns-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
+  selector: "ns-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
   moduleId: module.id,
 })
 export class AboutComponent implements OnInit {
-
-  constructor(private _router: Router) { }
+  constructor(private _router: Router) {}
 
   ngOnInit() {}
 
   getAbout() {
-    return localize('about_content').split('\n');
+    return localize("about_content").split("\n");
   }
 
   goBack() {
-    this._router.navigate(['home']); return;
+    this._router.navigate(["home"]);
+    return;
   }
-
 }
