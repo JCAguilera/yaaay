ee410968aa38ae78baff8c645a5ed953f8907f77
JuanCarlos Aguilera <jcaaguilerat@gmail.com>
2020-12-10 18:03:10 -0300

    Updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptLocalizeModule } from "nativescript-localize/angular";
 import { NativeScriptModule } from "@nativescript/angular";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
+
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./main/home/home.component";
-
-import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 import { ChangelogComponent } from "./main/changelog/changelog.component";
 import { AboutComponent } from "./main/about/about.component";
 
